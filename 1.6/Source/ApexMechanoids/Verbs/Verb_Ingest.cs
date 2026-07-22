e529d20cb9e85a3ac0503bafca046fadeb0fe635
Shashlichnik <dmv_01@mail.ru>
2026-07-23 05:02:20 +0600

    Targeting also uses ability comps Valid check

---
+++ 1.6/Source/ApexMechanoids/Verbs/Verb_Ingest.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ApexMechanoids
+{
+    public class Verb_Ingest : Verb_CastAbilityTouch
+    {
+        public override TargetingParameters targetParams
+        {
+            get
+            {
+                
+                if (base.targetParams.validator == null)
+                {
+                    base.targetParams.validator = IsValid_AbilityComps;
+                }
+                return base.targetParams;
+            }
+        }
+        protected bool IsValid_AbilityComps(TargetInfo target)
+        {
+            foreach (var abilityComp in ability.EffectComps)
+            {
+                if (!abilityComp.Valid((LocalTargetInfo)target))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
